40762fc6a3d489ee8cea21c70ba3ed5c07b7d791
williambrach <wibrach@gmail.com>
2023-04-01 21:15:14 +0200

    all add and categorie

--- application/main/components/Geom/controller.py
+++ application/main/components/Geom/controller.py
@@ -5,7 +5,8 @@ from application.main.infrastructure.sql import models
 import json
 from sqlalchemy.sql import text
 from geoalchemy2.types import Geography
-from sqlalchemy import or_, and_
+from sqlalchemy import or_, and_,distinct
+
 
 def transform_point_for_fe(x):
     r = dict(x)
@@ -16,20 +17,50 @@ def transform_point_for_fe(x):
     }
     return new
 
+def add(point, db):
+    new_geom = models.Aminity(
+                    fid = point.fid,
+                    aminity = point.aminity,
+                    lat = point.lat,
+                    lon = point.lon,
+                    addressline = point.addressline,
+                    type = point.type,
+                    info = point.info,
+                    geom = f'POINT({point.lon} {point.lat})'
+
+                    )
+
+    db.add(new_geom)   
+    db.commit()
+
+def search_all(db, category):
+    if category == None or category == "all":
+        category = get_categories(db)
+    print(category)
+    result = (
+        db.query(
+            models.Aminity.fid,
+            models.Aminity.aminity,
+            models.Aminity.lat,
+            models.Aminity.lon,
+            models.Aminity.name,
+            models.Aminity.type,
+            models.Aminity.addressline,
+            models.Aminity.info,
+        )
+        .filter(
+            models.Aminity.aminity.in_(category)    
+        ).all()
+    )
+    
+
+    points = [transform_point_for_fe(r) for r in result]
+
+    return points
 
 def search(db, lat, lon, radius, category):
     if category == None or category == "all":
-        category = [
-            "GreenPlace",
-            "Job",
-            "Shop",
-            "Sport",
-            "Drug store",
-            "culture",
-            "Transport",
-            "School",
-            "Hospital",
-        ]
+        category = get_categories(db)
     center_point = "POINT({lon} {lat})".format(lat=lat, lon=lon)
     result = (
         db.query(
@@ -59,3 +90,9 @@ def search(db, lat, lon, radius, category):
     points = [transform_point_for_fe(r) for r in result]
 
     return points
+
+def get_categories(db):
+
+    result = db.query(models.Aminity.aminity).distinct(models.Aminity.aminity).all()
+    result = [r['aminity'] for r in result]
+    return result
